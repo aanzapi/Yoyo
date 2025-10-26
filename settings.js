f008b4b04439fdcbdd372a00d0ed2f50d7df24fa
aanzapi <cuyaan08@gmail.com>
2025-10-26 17:08:47 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,9 @@
+// settings.js
+const config = {
+  api_id: "1774",
+  api_key: "yyqkkg-lbfn58-cy4aua-bbnmjj-4jdmix",
+  bot_token: "8089493197:AAG2QNzfIB7Cc8l6fiFmokUV9N5df-oJabg",
+  admin_id: "7878198899"
+};
+
+module.exports = { config };
